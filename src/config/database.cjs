f48a454515ae32110e68195f32932f312e0cac44
Duong Duc Anh <anhdd@bitecco.vn>
2023-06-06 17:29:57 +0700

    update

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -4,13 +4,14 @@ const host = process.env.DB_HOST;
 const username = process.env.DB_USER;
 const database = process.env.DB_NAME;
 const timezone = process.env.DB_TIMEZONE
+const port = process.env.DB_PORT
 
 module.exports = {
   development: {
     username,
     database,
     host,
-    port: 3306,
+    port: port,
     dialect: "mysql",
     query: {
       raw: true,
